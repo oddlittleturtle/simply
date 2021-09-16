5d6aacc0a0c5fc374992392409472082e1ea377d
oddlittleturtle <33937138+oddlittleturtle@users.noreply.github.com>
2021-09-16 02:40:14 -0500

    Update shorty.js

--- assets/scripts/shorty.js
+++ assets/scripts/shorty.js
@@ -11,3 +11,18 @@ fetch('https://api.short.io/links', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
+
+//Generate QR Code for short link
+
+const options = {
+  method: 'POST',
+  headers: {Accept: 'image/png', 'Content-Type': 'application/json'},
+  body: JSON.stringify({type: 'png'})
+};
+
+fetch('https://api.short.io/links/qr/linkIdString', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+//Todo: documentation for adding to Ghost webhook integrations and extra script to ghost footer
